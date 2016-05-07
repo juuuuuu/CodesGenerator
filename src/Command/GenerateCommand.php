e058d03fbdce25078e26db12c8df76048f1defae
julien.manganne <julien.manganne@extreme-sensio.com>
2016-05-08 00:27:39 +0200

    Add Xor Shift generator

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use mersenne_twister\twister;
+use Generator\XorShift;
 
 class GenerateCommand extends Command
 {
@@ -16,45 +16,56 @@ class GenerateCommand extends Command
             ->setName('generate:number')
             ->setDescription('Generate pseudo-random codes.')
             ->addArgument(
-                'number',
+                'seed',
                 InputArgument::OPTIONAL,
-                'How many number would you like to generate?',
-                10
+                'Which seed would you like to use?'
             )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $number = $input->getArgument('number');
-
         try {
             $memcache = new \Memcached();
             $memcache->addServer('localhost', 11211);
 
-            $length = 7;
+            $seed = $input->getArgument('seed');
+            $generator = new XorShift($seed);
+
+            $codeLength = 5;
             $possibleChars = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
-            $possibleCharsLength = strlen($possibleChars);
-            $previous = 0;
-            $generator = 13 ** 11;
-            $modulus = 7 ** 19;
 
-            for ($i = 0; $i < $number; ++$i) {
-                $previous = ($previous + $generator) % $modulus;
-                $code = '';
-                $temp = $previous;
+            for ($i = 0; $i < 10; ++$i) {
+                $randomValue = $generator->random();
 
-                for ($j = 0; $j < $length; ++$j) {
-                    $code .= $possibleChars[$temp % $possibleCharsLength];
-                    $temp = $temp / $possibleCharsLength;
+                while (strlen($randomValue) < ($codeLength * 2)) {
+                    $randomValue = $randomValue.mt_rand(0, 9);
                 }
 
-                if (!$memcache->get($code)) {
-                    $memcache->set($code, $code);
+                $split = str_split($randomValue);
 
-                    // @TODO: save the generated code somewhere (not a Database!)
-                    $output->writeln($code);
-                }
+                $map = [
+                    ($split[0].$split[1]) % 34,
+                    ($split[2].$split[3]) % 34,
+                    ($split[4].$split[5]) % 34,
+                    ($split[6].$split[7]) % 34,
+                    ($split[8].$split[9]) % 34,
+                ];
+
+                $code = $possibleChars[$map[0]].
+                        $possibleChars[$map[1]].
+                        $possibleChars[$map[2]].
+                        $possibleChars[$map[3]].
+                        $possibleChars[$map[4]];
+
+                $output->writeln($randomValue." ".$code);
+
+                // if (!$memcache->get($code)) {
+                //     $memcache->set($code, $code);
+
+                //     // @TODO: save the generated code somewhere (not a Database!)
+                //     $output->writeln($code);
+                // }
 
             }
 
@@ -66,6 +77,18 @@ class GenerateCommand extends Command
 
             return;
         }
+    }
 
+    private function randXorshift($number, $x, $y, $z)
+    {
+        $t = ( $x ^ ($x << 11) ) & 0x7fffffff;
+
+        $x = $y;
+        $y = $z;
+        $z = $number;
+
+        $number = ( $number ^ ($number >> 19) ^ ( $t ^ ( $t >> 8 )) );
+
+        return $number;
     }
 }
